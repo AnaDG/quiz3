4293f9449b11d259376afc265a15d98af906a229
AnaDG <anaidiazg@gmail.com>
2015-07-18 20:17:51 +0200

    Validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -45,7 +45,7 @@ exports.show = function(req, res){
 exports.answer = function(req, res){
 	var resultado = 'Incorrecto';
 	if (req.query.respuesta === req.quiz.respuesta){
-		resultado = 'Cocrrecto';
+		resultado = 'Correcto';
 	}
 	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado, errors: []});		
 };
